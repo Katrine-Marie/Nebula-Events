d60c15e593f0921e6469e221af66347b85d27252
Katrine-Marie <fjordscene@gmail.com>
2019-05-03 15:19:32 +0200

    Created common admin footer

---
+++ admin/views/admin-footer.php
@@ -0,0 +1,3 @@
+<div class="nebula-footer">
+    Plugin: Events plugin from <a href="https://fjordstudio.dk">Katrine-Marie Burmeister</a>  - <?php echo ' V'.nebula_EVENTS_VERS.' - &copy;'.date("Y"); ?>
+</div>
