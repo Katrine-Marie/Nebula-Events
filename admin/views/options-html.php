d812104e41a4da5d6b46587c6308edaefa37f86e
Katrine-Marie <fjordscene@gmail.com>
2019-05-06 16:38:16 +0200

    Cleaned up html

--- admin/views/options-html.php
+++ admin/views/options-html.php
@@ -1,36 +1,14 @@
-<?php
-/*
- * @since 0.10
- * Content for the options
- *
- */
-?>
-
-
-
-
 <div class="wrap admin-page nebula-options" >
 
     <h1 class="title"><?php echo esc_html( get_admin_page_title() ); ?></h1>
 
     <form method="post" action="<?php echo esc_html(admin_url('admin-post.php')); ?>">
 
-
-
-
-        <!-- <label for="user-checkbox" class="regular-number">Checkbox: </label>
-        <input type="checkbox" name="user-checkbox" value="1" <?php // echo $this->GetCheckbox('user-checkbox'); ?> >
-        <br/> -->
-        <!-- <label for="plugin-serial" class="regular-number">Serial Code: </label>
-        <input type="text" name="plugin-serial" value="<?php // echo $this->GetOption('plugin-serial'); ?>" >
-<br/> -->
-         <label for="single-layout">Single Page</label><select name="single-layout">
-         <!-- <option <?php // echo $this->GetSelected('single-layout', 'shortcode'); ?> value="shortcode" >Shortcode</option> -->
-         <option <?php echo $this->GetSelected('single-layout', 'template1'); ?> value="template1" >Simple Template</option>
-         <option <?php echo $this->GetSelected('single-layout', 'content1'); ?> value="content1" >Replace Content Template</option>
+         <label for="single-layout">Single Page</label>
+         <select name="single-layout">
+           <option <?php echo $this->GetSelected('single-layout', 'template1'); ?> value="template1" >Simple Template</option>
+           <option <?php echo $this->GetSelected('single-layout', 'content1'); ?> value="content1" >Replace Content Template</option>
          </select>
-
-
 
         <input type="hidden" name="action" value="<?php echo $this->myaction; ?>">
         <input type="hidden" name="version" value="<?php echo $this->GetOption('version'); ?>">
