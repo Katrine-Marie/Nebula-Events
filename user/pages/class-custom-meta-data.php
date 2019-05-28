b921f749d6d2e0e39c4939e96dfbd30be5f9a5d3
Katrine-Marie <fjordscene@gmail.com>
2019-05-28 15:14:27 +0200

    Cleaned up file

--- user/pages/class-custom-meta-data.php
+++ user/pages/class-custom-meta-data.php
@@ -7,99 +7,94 @@
  *    data field prefix
  *    type: meta, options
  */
+
 namespace nebula\events;
+
 if (!class_exists('custom_meta_data')) {
 
+  class custom_meta_data {
 
-    class custom_meta_data {
+    public function __construct ( $data_name, $prefix, $meta  )  {
+      global $post;
+      $this->meta_options = array();
+      $this->default_meta_options = array();
+      $this->nebula_prefix = $prefix;
+      $this->nebula_data_name = $data_name;
+      // $this->LoadOptions($meta,$post->ID);
+      $this->meta = $meta;
+    }
 
+    /**
+    *  Loads options from database.
+    * loops through Only values that already known in the settings.
+    * does not take advantage of the default options.
+    * and loads any stored values
+    * get_post_meta($post->ID, $this->nebula_data_name, true))
+    */
+    function LoadOptions ( $post_id = 0 ) {
+      global $post;
+      switch ($this->meta){
+        case 'meta':
 
-        public function __construct ( $data_name, $prefix, $meta  )  {
-            global $post;
-            $this->meta_options = array();
-            $this->default_meta_options = array();
-            $this->nebula_prefix = $prefix;
-            $this->nebula_data_name = $data_name;
-            // $this->LoadOptions($meta,$post->ID);
-            $this->meta = $meta;
+        $storedoptions = get_post_meta($post->ID, $this->nebula_data_name, true);
+        if ($storedoptions && is_array($storedoptions)) {
+          foreach ($storedoptions as $key => $value) {
+            $this->meta_options[$key] = $value;
+          }
         }
+        break;
+        case 'options':
 
-        /**
-         *  Loads options from database.
-         * loops through Only values that already known in the settings.
-         * does not take advantage of the default options.
-         * and loads any stored values
-         * get_post_meta($post->ID, $this->nebula_data_name, true))
-         */
-        function LoadOptions ( $post_id = 0 ) {
-            global $post;
-            switch ($this->meta)
-            {
-                case 'meta':
-                // bring in loaded values
-                $storedoptions = get_post_meta($post->ID, $this->nebula_data_name, true);
-                if ($storedoptions && is_array($storedoptions)) {
-                    foreach ($storedoptions as $key => $value) {
-                        $this->meta_options[$key] = $value;
-                    }
-                }
-                break;
-                case 'options':
-
-                $storedoptions = get_option($this->nebula_data_name);
-                if ($storedoptions && is_array($storedoptions)) {
-                 foreach ($storedoptions as $key => $value) {
-                 $this->meta_options[$key] = $value;
-                 }
-               }
-               break;
-           default :
-               return;
-            }
-
+        $storedoptions = get_option($this->nebula_data_name);
+        if ($storedoptions && is_array($storedoptions)) {
+          foreach ($storedoptions as $key => $value) {
+            $this->meta_options[$key] = $value;
+          }
         }
+        break;
+        default :
+        return;
+      }
 
+    }
 
+    /**
+    *  Returns option value for given key
+    *  from array already loaded
+    */
+    public function GetMetaOption($key) {
+      $key = $this->nebula_prefix . $key;
+      if (array_key_exists($key, $this->meta_options)) {
+        return $this->meta_options[$key];
+      } else
+       return null;
+    }
 
-
-     /**
-      *  Returns option value for given key
-      *  from array already loaded
-      */
-     public function GetMetaOption($key) {
-         $key = $this->nebula_prefix . $key;
-         if (array_key_exists($key, $this->meta_options)) {
-             return $this->meta_options[$key];
-         } else
-             return null;
-     }
-    /*
-     * echo out content plus BR or nothing if empty
-     */
     public function ShowOption($key) {
-               $key = $this->nebula_prefix . $key;
-               if (array_key_exists($key, $this->meta_options)) {
-                   if (strlen($this->meta_options[$key])>1) {
-                       return $this->meta_options[$key].'<br/>';
-                   }
-               } else
-                   return null;
-           }
+      $key = $this->nebula_prefix . $key;
+      if (array_key_exists($key, $this->meta_options)) {
+        if (strlen($this->meta_options[$key])>1) {
+          return $this->meta_options[$key].'<br/>';
+        }
+      } else
+      return null;
+    }
+
     public function Optionhasvalue($key) {
-               $key = $this->nebula_prefix . $key;
-               if (array_key_exists($key, $this->meta_options)) {
-                   if (strlen($this->meta_options[$key])>1) {
-                       return true;
-                   }
-               }
-               return false;
-           }
+      $key = $this->nebula_prefix . $key;
+      if (array_key_exists($key, $this->meta_options)) {
+        if (strlen($this->meta_options[$key])>1) {
+          return true;
+        }
+      }
+      return false;
+    }
+
     public function test() {
       global $post;
       echo "test". $post->ID;
     }
 
-
-    } //end Class
+  }
 
 }
